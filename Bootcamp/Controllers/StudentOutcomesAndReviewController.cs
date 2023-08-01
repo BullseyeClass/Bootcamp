261ff1871756566eb0873ac57d8259524977c36c
Olawole Temitope <Olawole.T@Bullseyemanpower.com>
2023-08-01 22:22:40 +0100

    Controller, view folder and view file created

---
+++ Bootcamp/Controllers/StudentOutcomesAndReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp.Controllers
+{
+    public class StudentOutcomeAndReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
