8dc88a2dbd7ea9b81cfb03d35b261b2b80170f58
Okwe Anthonia <Okwe.A@Bullseyemanpower.com>
2023-08-07 12:18:23 +0100

    update

---
+++ Bootcamp/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp.Controllers
+{
+    public class blogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
