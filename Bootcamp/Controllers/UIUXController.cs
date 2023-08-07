4c2def84465a350c021fe374bb26314c461b705c
Ogbebor.S <Ogbebor.S@BULLSEYE-PC18>
2023-08-07 09:45:14 +0100

    update

---
+++ Bootcamp/Controllers/UIUXController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp.Controllers
+{
+    public class UIUXController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
