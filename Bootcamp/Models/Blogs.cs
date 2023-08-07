f3d788da5674593038e25f44e9616efa9c40ac8d
Okwe Anthonia <Okwe.A@Bullseyemanpower.com>
2023-08-07 16:24:58 +0100

    update

--- Bootcamp/Models/Blogs.cs
+++ Bootcamp/Models/Blogs.cs
@@ -2,5 +2,14 @@
 {
     public class Blogs
     {
+        public string Id { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string[] Category { get; set; }
+        public string[]? SubCategory { get; set; }
+        public string Link { get; set; }
+        public bool StartHere { get; set; }
+        public bool MostRecent { get; set; }
     }
 }
