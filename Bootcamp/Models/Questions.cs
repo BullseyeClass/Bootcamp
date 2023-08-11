915489d71478023bac078cd65b0ebb33bb5a4b70
Emmanuel Ogidan <88818922+Emogibam@users.noreply.github.com>
2023-08-11 19:11:36 +0100

    adding questions Model

---
+++ Bootcamp/Models/Questions.cs
@@ -0,0 +1,19 @@
+﻿namespace Bootcamp.Models
+{
+    public class Questions
+    {
+
+        public class Rootobject
+        {
+            public Class1[] Property1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public string question { get; set; }
+            public string[] options { get; set; }
+            public string correctAnswer { get; set; }
+        }
+
+    }
+}
