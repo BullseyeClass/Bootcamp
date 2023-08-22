20fb47a4e7c1851b06ee04098cfee1a65fe27816
Olawole Temitope <Olawole.T@Bullseyemanpower.com>
2023-08-22 19:08:32 +0100

    Dashboard Model

---
+++ Bootcamp/Models/Dashboard/DashBoard.cs
@@ -0,0 +1,10 @@
+﻿namespace Bootcamp.Models.Dashboard
+{
+    public class DashBoard
+    {
+        public string TestType { get; set; } = string.Empty;
+        public string Score { get; set; } = string.Empty;   
+        public bool IsPassed { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
