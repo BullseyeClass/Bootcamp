2862e03cde8174da1242f66963ba032759be99e6
Olorunshola Joshua <Olorunshola.J@bullseye.local>
2023-08-24 12:29:54 +0100

    changs

--- Bootcamp/Controllers/DashboardController.cs
+++ Bootcamp/Controllers/DashboardController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bootcamp.Models.ApiResponse;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.Security.Claims;
 
 namespace Bootcamp.Controllers
 {
-    public class DashboardController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class DashboardController : Controller
+	{
+		private readonly IConfiguration _configuration;
+		private readonly string _baseUrl;
+
+
+
+		public DashboardController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			_baseUrl = _configuration["AppSettings:BaseUrl"];
+		}
+		public async Task<IActionResult> Index()
+		{
+			var userId = HttpContext.User.FindFirst(x => x.Type == ClaimTypes.NameIdentifier).Value;
+			using (var httpClient = new HttpClient())
+			{
+				try
+				{
+					HttpResponseMessage response = await httpClient.GetAsync($"{_baseUrl}/api/GetUser/{userId}");
+					if (response.IsSuccessStatusCode)
+					{
+						string responseBody = await response.Content.ReadAsStringAsync();
+						List<ApiResponseModel> traineeDetails = JsonConvert.DeserializeObject<List<ApiResponseModel>>(responseBody);
+						//var details = traineeDetails.Where(x => x.TraineeId == Guid.Parse(userId));
+						return View(traineeDetails);
+					}
+					else
+					{
+						//Handle error case
+						return RedirectToAction("Index", "Error");
+					}
+				}
+				catch (Exception ex)
+				{
+					return RedirectToAction("Index", "Error");
+				}
+				
+			}
+		}
+	}
 }
---
+++ Bootcamp/Models/ResponseFolder/DashboardResponseModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Bootcamp.Models.ApiResponse
+{
+	public class PhoneNumber
+	{
+		public string Extension { get; set; }
+		public string Number { get; set; }
+	}
+
+	public class Address
+	{
+		public string PostalCode { get; set; }
+		public string MainAddress { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Country { get; set; }
+	}
+
+	public class Test
+	{
+		public int? Id { get; set; }
+		public string TestType { get; set; }
+		public int Score { get; set; }
+		public bool IsPassed { get; set; }
+	}
+
+	public class ApiResponseModel
+	{
+		public Guid TraineeId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string MiddleName { get; set; }
+		public List<PhoneNumber> PhoneNumbers { get; set; }
+		public List<Address> Addresses { get; set; }
+		public List<Test> Tests { get; set; }
+	}
+}
